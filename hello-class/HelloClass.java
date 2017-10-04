8f67dde25252fb1cebdfb87f2982b78826b143c1
Andy Makar <andrewjmakar@gmail.com>
2017-10-03 22:53:59 -0400

    Added package to HelloClass

--- hello-class/HelloClass.java
+++ hello-class/HelloClass.java
@@ -2,6 +2,7 @@
 Description: Prints Hello Name for each student in the class
 * Added Instructor Name and Prefix
 */
+package helloclass;
 
 public class HelloClass {
   public static void main(String[] args) {
