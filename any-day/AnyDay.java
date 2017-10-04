0b6d4f7d7013a82f1700f1d267c004ee8edd0bb4
Andy Makar <andrewjmakar@gmail.com>
2017-10-03 22:51:39 -0400

    Added package to resolve JAR quality issue

--- any-day/AnyDay.java
+++ any-day/AnyDay.java
@@ -4,6 +4,7 @@ Date: 9/25/17
 Program Description: My implementation of Zellers Congruence
 Source Algorithm: https://en.wikipedia.org/wiki/Zeller%27s_congruence
 */
+package anyday;
 
 import java.util.*;
 
@@ -11,8 +12,12 @@ public class AnyDay {
     public static void main(String[] args) {
         // TODO code application logic here   
         Scanner input = new Scanner(System.in);    
-        int day, month, year = 0;
-        int h,k,j = 0;
+        int day = 0;
+		int month = 0;
+		int year = 0;
+        int h = 0;
+		int k = 0;
+		int j = 0;
 
         System.out.print("Enter the month (1-12): ");    
         month = input.nextInt();
@@ -51,7 +56,7 @@ public class AnyDay {
             case 4 : System.out.println("Wednesday"); break;
             case 5 : System.out.println("Thursday"); break;
             case 6 : System.out.println("Friday"); break;
-			default: System.out.println("Invalid Day Calculation");
+			default: System.out.println("Invalid Day Calculation"); break;
         }
      
         
