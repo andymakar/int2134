70add0901ac7946fea8aa4bc7c762339cedd6024
AMAKAR4 <AMAKAR4@WGC1WCDQV1L72.foc.ford.com>
2018-10-08 08:26:17 -0400

    changed instructor name

--- hello-class/HelloClass.java
+++ hello-class/HelloClass.java
@@ -2,13 +2,13 @@
 Description: Prints Hello Name for each student in the class
 * Added Instructor Name and Prefix
 */
-package helloclass;
+
 
 public class HelloClass {
   public static void main(String[] args) {
     
     System.out.println("INT2134 Class Roster: ");
-	System.out.println("Dr. Andrew Makar - Adjunct Professor");
+	System.out.println("Dr. Andrew Makar, esq - Adjunct Professor");
 
   }
 }
