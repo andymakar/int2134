2040e76cbe9e8567b0af5420a4f2e83c02d3b221
Nsylvia99 <35315617+Nsylvia99@users.noreply.github.com>
2018-10-15 19:55:49 -0400

    Created intAt method
    if you input a set of number and the position then you will recieve the number in that position as an integer.
    Format:
    
    .intAt(String, Position)

---
+++ students/Nathan-Sylvia/IntAt/src/intat/IntAt.java
@@ -0,0 +1,14 @@
+/*
+ * Nathan Sylvia 
+ * Class: IntAt
+ * 10-15-18
+*/ 
+package intat;
+public class IntAt {
+    public static int intAt(String number, int n) {
+        //seperate the character that you want
+        char charEnd = number.charAt(n);
+        int intEnd = (int) (charEnd - '0');
+        return intEnd;
+    }
+}
