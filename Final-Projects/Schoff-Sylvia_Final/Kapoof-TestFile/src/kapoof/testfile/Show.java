fd0999da243e5ed847a194387912f95be9584978
Nsylvia99 <35315617+Nsylvia99@users.noreply.github.com>
2018-12-04 18:50:18 -0500

    Update Show.java

--- Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/Show.java
+++ Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/Show.java
@@ -18,7 +18,13 @@ public class Show {
     private String client;
     private String location;
     Show() {
-        
+        this.title = "Unknown";
+        this.type = 0;
+        this.performanceDate = "Unknown";
+        this.bookingDate = "Unknown";
+        this.fee = 0;
+        this.client = "Unknown";
+        this.location = "Unknown";
     }
     Show(String title, int type, String performanceDate, String bookingDate, int fee, String client, String location) {
         this.title = title;
@@ -28,5 +34,16 @@ public class Show {
         this.fee = fee;
         this.client = client;
         this.location = location;
+    }
+    public void dropStudent(String student, int i) { 
+        if (student.equalsIgnoreCase(students[i])) {
+            students[i] = null; // sets dropped student's value to null
+            numberOfStudents--;
+            while (i < numberOfStudents) {
+                students[i] = students[i + 1];
+                i++;
+            }
+            }
+        }
     }
 }
