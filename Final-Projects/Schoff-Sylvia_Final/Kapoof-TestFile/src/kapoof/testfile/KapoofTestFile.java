e724e094b825b86517ea474c7cc562665bb44fd7
Nsylvia99 <35315617+Nsylvia99@users.noreply.github.com>
2018-12-04 18:24:06 -0500

    Making it pretty...

--- Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/KapoofTestFile.java
+++ Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/KapoofTestFile.java
@@ -1,21 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * Nathan S & Zachary S
+ * Final project
+ * Kapoof test file
+ * 12/4/2018
+*/
 package kapoof.testfile;
-
-/**
- *
- * @author nsylvia
- */
+import java.util.*;
 public class KapoofTestFile {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
