c43bbe61469eb8a442e2d0ff380dad989e05ed4e
SchoffZ <zschoff@ltu.edu>
2018-12-05 18:46:56 -0500

    Streamlined futher

--- Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/MainMenu.java
+++ Final-Projects/Schoff-Sylvia_Final/Kapoof-TestFile/src/kapoof/testfile/MainMenu.java
@@ -16,8 +16,8 @@ public class MainMenu {
         Client[] listOfClients = new Client[10];
         Show[] listOfShows = new Show[10];
         //CHANGE WHEN WE ARE DONE WITH CODE
-        Client client = new Client();
-        Show show = new Show();
+        //Client client = new Client();
+        //Show show = new Show();
         //CHANGE WHEN WE ARE DONE WITH CODE
         System.out.println("___________________________________________\nPlease enter a selection:\n1) Add a client\n2) Update a client\n3) Delete a client\n4) Add a show\n5) Update a show\n6) Delete a show\n7) Report list of clients\n8) Report list of shows for the month\n9) Exit\n___________________________________________");
         int selection = input.nextInt();
@@ -25,8 +25,8 @@ public class MainMenu {
             System.out.println("___________________________________________\nPlease enter a selection:\n1) Add a client\n2) Update a client\n3) Delete a client\n4) Add a show\n5) Update a show\n6) Delete a show\n7) Report list of clients\n8) Report list of shows for the month\n9) Exit\n___________________________________________");
             switch(selection){
                 case 1: System.out.println("Please input the client's first name, last name, phone number, address and email; int that order: ");
-                    listOfClients[client.getCount()] = new Client(input.next(), input.next(), input.next(), input.next(), input.next());
-                    System.out.println(listOfClients[client.getCount()]);
+                    listOfClients[listOfClients[0].getCount()] = new Client(input.next(), input.next(), input.next(), input.next(), input.next());
+                    System.out.println(listOfClients[listOfClients[0].getCount()]);
                     break;
                 case 2: System.out.println("Case 2");
                     break;
